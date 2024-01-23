b0de91138856f34ea977570d9b5addeb711f12f2
akuaataa <priscyagyemang60@gmail.com>
2024-01-23 14:49:25 -0800

    Added an index.js

---
+++ src/index.js
@@ -0,0 +1,39 @@
+function showTemperature(response) {
+  let tempValueElement = document.querySelector("#temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  tempValueElement.innerHTML = `${temperature}`;
+
+  // Update the temperature image based on the temperature
+  let temperatureImageElement = document.querySelector("#temperature-image");
+  updateTemperatureImage(temperatureImageElement, temperature);
+
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function updateTemperatureImage(imageElement, temperature) {
+  if (temperature < 10) {
+    imageElement.src =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/111/827/original/snow-day_%281%29.png?1706026047";
+  } else if (temperature > 30) {
+    imageElement.src =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/111/880/original/OIP.jpg?1706049114";
+  } else {
+    imageElement.src =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/111/826/original/clear-sky-day.png?1706026013";
+  }
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  let apiKey = "117da7948a04330f7fb96d315918tcdo";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+// Add an event listener to the search form
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
